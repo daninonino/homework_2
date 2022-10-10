ffa415239912914c74c385f907a3b61447d5d3d2
George8080 <josantis2021@inf.udec.cl>
2022-10-10 18:04:49 -0300

    Cambio de nombre

--- Maquina/src/maquina/Maquina.java
+++ Maquina/src/maquina/Maquina.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package expendedora;
+package maquina;
 
 /**
  *
  * @author jorge
  */
-public class Expendedora {
+public class Maquina {
 
     /**
      * @param args the command line arguments
