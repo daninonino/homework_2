59aeb69e5a438c5a3bd2e320b2f03d1990342e79
daninonino <danielanovoao600@gmail.com>
2022-10-10 21:05:15 -0300

    avances en comprador (finalizado?)

--- Maquina/src/maquina/Comprador.java
+++ Maquina/src/maquina/Comprador.java
@@ -1,21 +1,29 @@
-
 package maquina;
-
+import java.util.ArrayList;
 
 public class Comprador {
     private int cantidadTotal;
     private String sabor;
+    private ArrayList<Moneda> moneda;
+    private Bebida bebida;
     
-    public Comprador(Moneda mon, int tipo, Expendedor exp){ 
+    
+    public Comprador(Moneda mon, int tipoBebida, Expendedor exp){ 
+        ArrayList<Moneda> moneda=new ArrayList<Moneda>();
+        moneda.add(mon);
         cantidadTotal = mon.getValor();
-        cantidadTotal -= 
+        bebida = exp.ComprarBebida(tipoBebida, mon);
         
     }
+    public int cantidadTotal(){
+        return cantidadTotal;
+    }
+    
     public int cuantoVuelto(){
         return getVuelto();
     } 
     public String queBebiste(){
-        return "gluglu: " + beber;
+        return "gluglu: " + bebida.beber();
     }
 
 }
