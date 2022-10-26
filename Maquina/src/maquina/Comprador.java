47c8345a8e9ea8b84a665fc2680c44669bafd89d
George8080 <josantis2021@inf.udec.cl>
2022-10-26 00:49:17 -0300

    Eliminación de throw innecesario

--- Maquina/src/maquina/Comprador.java
+++ Maquina/src/maquina/Comprador.java
@@ -4,7 +4,7 @@ public class Comprador {
     private int cantidadTotal;
     private String sabor;
 
-    public Comprador(Moneda mon, int tipoBebida, Expendedor exp) throws PagoInsuficienteException, PagoIncorrectoException, NoHayBebidaException{ 
+    public Comprador(Moneda mon, int tipoBebida, Expendedor exp){ 
         Bebida bebida;
         sabor = null;
         try{
